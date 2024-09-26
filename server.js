bcd56ea1d02e2feb412c7f4418426d5b18f760e5
Shk123 <shaqeebsk1234@gmail.com>
2024-09-26 08:17:24 +0530

    db added

--- server.js
+++ server.js
@@ -1,48 +1,89 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const { createWorker } = require("tesseract.js");
-const { Bot } = require('grammy');
+const { Keyboard } = require("grammy");
+const { Bot } = require("grammy");
 const token = process.env.TOKEN;
-const bot = new Bot(token);  
-
+const bot = new Bot(token);
+const connection = require("./database");
+connection.connect();
 
 const textExacter = async (imagePath) => {
   try {
-    const worker = await createWorker(["eng","hin"]);
-    const { data: { text } } = await worker.recognize(imagePath);
+    const worker = await createWorker(["eng", "hin"]);
+    const {
+      data: { text },
+    } = await worker.recognize(imagePath);
     await worker.terminate();
-    return text; 
+    return text;
   } catch (error) {
     console.log("Tesseract error:", error);
     return null;
   }
 };
 
-bot.command('start', (ctx) => {
-  const introductionMessage = `Hello ${ctx.update.message.from.first_name}! I'm a Telegram bot.
-I'm powered by shaqeeb, the next-generation serverless computing platform for Budget Management System.
-Please send me an image, and I'll try to extract the text from it.`;
-  ctx.reply(introductionMessage);  
+bot.command("start", async (ctx) => {
+  const userId = ctx.update.message.from.id;
+  const username = ctx.update.message.from.username;
+  const firstName = ctx.update.message.from.first_name;
+  
+  if (userId) {
+    try {
+  
+      const selectQuery = "SELECT * FROM userTextBot WHERE user_id = ?";
+      const [existingUser] = await connection.promise().execute(selectQuery, [userId]);
+  
+      if (existingUser.length > 0) {
+        const userInfo = existingUser[0];  
+        const userMessage = `Welcome back ${userInfo.first_name}! `;
+        ctx.reply(userMessage);
+      } else {
+      
+        const insertQuery = "INSERT INTO userTextBot (user_id, username, first_name) VALUES (?, ?, ?)";
+        const [userIntro] = await connection.promise().execute(insertQuery, [userId, username, firstName]);
+  
+        if (userIntro.affectedRows == 1) {
+          const introductionMessage = `Hello ${firstName}! I'm a Telegram bot.
+          I'm powered by shaqeeb, the next-generation serverless computing platform for Budget Management System.
+          Please send me an image, and I'll try to extract the text from it.`;
+          ctx.reply(introductionMessage);
+        }
+      }
+    } catch (error) {
+      console.error("Database operation failed:", error);
+      ctx.reply("There was an error processing your request. Please try again later.");
+    }
+  }
 });
 
+bot.on("message", async (ctx) => {
+    const userId = ctx.update.message.from.id;
+  if (ctx.message.photo) {
+    const fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
 
-bot.on('message', async (ctx) => {
-  if (ctx.message.photo) {  
-    const fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id; 
-    
     try {
-   
       const file = await bot.api.getFile(fileId);
-      const filePath = `https://api.telegram.org/file/bot${token}/${file.file_path}`;  
-  
+      const filePath = `https://api.telegram.org/file/bot${token}/${file.file_path}`;
+
       const recognizedText = await textExacter(filePath);
 
       if (recognizedText) {
-        await ctx.reply(`I found the following text in your image: \n${recognizedText}`);
-        
-        await ctx.reply("Thank you for using me!!");
-        
+      
+        const selectQuery = "SELECT * FROM userTextBot WHERE user_id = ?";
+        const [existingUser] = await connection.promise().execute(selectQuery, [userId]);
+
+        if (existingUser.length > 0) {
+  
+          const updateQuery = "UPDATE userTextBot SET text = ?  WHERE user_id = ?";
+          await connection.promise().execute(updateQuery, [recognizedText , userId]);
+
+          await ctx.reply(`I found the following text in your image: \n${recognizedText}`);
+          await ctx.reply("Your information has been updated. Thank you for using me!");
+        } else {
+             const text = "/start " + "," +  " New User! Please Click on start."
+          ctx.reply(text);
+        }
       } else {
         await ctx.reply("Sorry, I couldn't extract any text from this image.");
       }
@@ -53,6 +94,20 @@ bot.on('message', async (ctx) => {
   } else {
     ctx.reply("Please send a photo for me to process!");
   }
+});
+
+
+bot.command("request_contact", (ctx) => {
+  const keyboard = new Keyboard().requestContact("Share your phone number");
+
+  ctx.reply("Please share your phone number:", {
+    reply_markup: {},
+  });
+});
+
+bot.on("message:contact", (ctx) => {
+  const phoneNumber = ctx.message.contact.phone_number;
+  ctx.reply(`Thanks for sharing your phone number: ${phoneNumber}`);
 });
 
 bot.start();
