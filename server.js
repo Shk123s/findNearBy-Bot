cdb6f8310446e11526d1ebde0e9fc2e1dbc188ec
shaqeebshaikh123 <90406905+Shk123s@users.noreply.github.com>
2024-12-25 10:52:14 +0530

    Update server.js health check

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const dbconnection = require("./database");
 const mainRoutes = require("./routes/routes");
 const botCaller = require("./functions/botFunctions");
 
+app.get('/healthCheck', async (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: '🚀 Backend Service is Up and Running! 💻🌟',
+      dBHealthCheck: '✅ PASS 🗄️',
+      status: 'success',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Health check failed!',
+      error: error.message,
+    });
+  }
+});
+
 app.use("/bot/api/v1",mainRoutes);
 
 //start the bot.
