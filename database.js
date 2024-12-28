c827bec5eb6f37d594cfdc38d80b3dd13f547c87
shaqeebshaikh123 <90406905+Shk123s@users.noreply.github.com>
2024-12-28 10:42:20 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,28 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+
 const connection = mysql.createConnection({
   host: process.env.db_LOCALHOST,
   user: process.env.db_USERNAME,
   password: process.env.db_PASSWORD,
   database: process.env.db_DATABASE,
   port: process.env.db_PORT,
-  connectTimeout: 10000, 
+  connectTimeout: 20000, // Increased timeout to 20 seconds
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+    connection.end();
+    return;
+  }
   console.log("db Connected!");
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  connection.end();
+  process.exit(1);
 });
 
 module.exports = connection;
